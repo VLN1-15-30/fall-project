63575e89e4f1fbf46fb68bb6ab5be2fb9e617b1d
Gunnar Bjarki Bjornsson <gunnar@stokkur.is>
2015-11-27 12:20:20 +0000

    Added person class

---
+++ hooper/person.cpp
@@ -0,0 +1,74 @@
+
+#include "person.h"
+
+using namespace std;
+
+person::person(){
+
+    born = 0;
+    died = 0;
+    name = "";
+    sex = "";
+}
+
+string person:: getName(){
+
+    return name;
+}
+
+int person:: getBorn(){
+    return born;
+}
+
+int person:: getDied(){
+    return died;
+}
+
+string person:: getSex(){
+    return sex;
+}
+
+void person:: setName(string n){
+    name = n;
+}
+
+void person:: setSex(string s){
+    sex = s;
+}
+
+void person:: setBorn(int b){
+    born = b;
+}
+
+void person:: setDied(int d){
+
+    died = d;
+}
+
+
+void person::initialize(){
+
+    cout << "Name: ";
+    cin >> name;
+    cout << endl;
+    cout << "Sex: ";
+    cin >> sex;
+    cout << endl;
+    cout << "Born: ";
+    cin >>born;
+    cout << endl;
+    cout << "Died: ";
+    cin >> died;
+    cout << endl;
+
+}
+
+ostream& operator<< (ostream& stream,const person& p){
+
+   stream << "Name: "<< p.name<<endl;
+   stream << "Sex: "<< p.sex<<endl;
+   stream << "Born: "<< p.born<<endl;
+   stream << "Died: "<< p.died<<endl;
+
+   return stream;
+}
---
+++ hooper/person.h
@@ -0,0 +1,37 @@
+#ifndef PERSON
+#define PERSON
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class person
+{
+public:
+    person();
+    void initialize();
+    //friend istream& operator>> (istream& stream,const person& p);
+    friend ostream& operator<< (ostream& stream,const person& p);
+
+    string getName();
+    string getSex();
+    int getBorn();
+    int getDied();
+
+    void setName(string n);
+    void setSex(string s);
+    void setBorn(int b);
+    void setDied(int d);
+
+
+private:
+    int born;
+    int died;
+    string name;
+    string sex;
+
+};
+
+#endif // PERSON
