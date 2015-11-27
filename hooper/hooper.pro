0842f9cbc5556b326b133f7a1f9131140184ea0b
sverrirp15 <sverrirp15git config --global user.email sverrirp15@ru.is>
2015-11-27 14:17:09 +0000

    uppdated main and pro files

--- hooper/hooper.pro
+++ hooper/hooper.pro
@@ -8,8 +8,10 @@ CONFIG -= app_bundle
 TEMPLATE = app
 
 SOURCES += main.cpp \
-    person.cpp
+    person.cpp \
+    list.cpp
 
 HEADERS += \
-    person.h
+    person.h \
+    list.h
 
--- hooper/main.cpp
+++ hooper/main.cpp
@@ -1,11 +1,15 @@
 #include <QCoreApplication>
 #include <iostream>
+#include "list.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Welcome to Hooper" << endl;
+    List str1;
+    str1.initialize("persons.txt");
+    str1.addData();
+    cout << str1;
     return 0;
 }
 
